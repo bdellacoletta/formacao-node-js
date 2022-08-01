e2a86181445a8071c8d3d57f28954e3ef9696041
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-08-01 16:25:24 -0300

    adicionado parâmetro opcional na rota de 'blog'

--- express-js/index.js
+++ express-js/index.js
@@ -5,21 +5,26 @@ const app = express(); //Iniciando o express
 // RES => RESPOSTA QUE VAI SER ENVIADA PARA O USUÁRIO
 
 app.get("/", function(req, res){  //Criando rota de home(localhost:8080/)
-  res.send("<h1>Bem-vindo!/h1>")
+  res.send("<h1>Bem-vindo!/h1>");
 });
 
-app.get("/blog", function(req, res){  //Criando rota de blog(localhost:8080/blog)
-  res.send("<h1>Bem-vindo ao meu blog!</h1>")
+app.get("/blog/:artigo?", function(req, res){  //Criando rota de blog com parâmetro opcional 'artigo'(localhost:8080/blog ou localhost:8080/blog/:artigo)
+  var artigo = req.params.artigo;
+  if (artigo){
+    res.send(`<h2>Artigo: ${artigo}`);
+  } else {
+    res.send("<h1>Bem-vindo ao meu blog!</h1>");
+  }
 });
 
 app.get("/canal/youtube", function(req, res){  //Criando rota de canal/youtube(localhost:8080/canal/youtube)
-  res.send("<h1>Bem-vindo ao meu canal!</h1>")
+  res.send("<h1>Bem-vindo ao meu canal!</h1>");
 });
 
-app.get("/ola/:nome/:empresa", function(req, res){  //Criando rota de ola com parâmetros 'nome' e 'empresa' (localhost:8080/ola/:nome/:empresa)
+app.get("/ola/:nome/:empresa", function(req, res){  //Criando rota de ola com parâmetros obrigatórios 'nome' e 'empresa' (localhost:8080/ola/:nome/:empresa)
   var nome = req.params.nome;
   var empresa = req.params.empresa;
-  res.send(`<h1>Olá ${nome} da empresa ${empresa}</h1>`)
+  res.send(`<h1>Olá ${nome} da empresa ${empresa}</h1>`);
 });
 
 app.listen(8080, function(erro) { //adicioando número da porta para acesso ao localhost
