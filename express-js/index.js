899777e8776fd98d23c25e756210a55382648ad7
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-08-01 16:22:20 -0300

    adicionado parâmetros obrigatórios na rota de 'ola'

--- express-js/index.js
+++ express-js/index.js
@@ -1,19 +1,28 @@
 const express = require("express"); //Importando o express
 const app = express(); //Iniciando o express
 
-app.get("/", function(req, res){
-  res.send("<h1>Bem-vindo!</h1>")
+// REQ => DADOS ENVIADOS PELO USUÁRIO
+// RES => RESPOSTA QUE VAI SER ENVIADA PARA O USUÁRIO
+
+app.get("/", function(req, res){  //Criando rota de home(localhost:8080/)
+  res.send("<h1>Bem-vindo!/h1>")
 });
 
-app.get("/blog", function(req, res){
+app.get("/blog", function(req, res){  //Criando rota de blog(localhost:8080/blog)
   res.send("<h1>Bem-vindo ao meu blog!</h1>")
 });
 
-app.get("/canal/youtube", function(req, res){
+app.get("/canal/youtube", function(req, res){  //Criando rota de canal/youtube(localhost:8080/canal/youtube)
   res.send("<h1>Bem-vindo ao meu canal!</h1>")
 });
 
-app.listen(8080, function(erro) {
+app.get("/ola/:nome/:empresa", function(req, res){  //Criando rota de ola com parâmetros 'nome' e 'empresa' (localhost:8080/ola/:nome/:empresa)
+  var nome = req.params.nome;
+  var empresa = req.params.empresa;
+  res.send(`<h1>Olá ${nome} da empresa ${empresa}</h1>`)
+});
+
+app.listen(8080, function(erro) { //adicioando número da porta para acesso ao localhost
   if(erro){
     console.log("Ocorreu um erro!");
   } else{
