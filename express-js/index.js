503b7fe8f8e80c3f39f677b81389792d27350d4b
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-08-01 16:04:35 -0300

    instalado 'nodemon' para reiniciar o servidor automaticamente quando houver alteração

--- express-js/index.js
+++ express-js/index.js
@@ -17,6 +17,6 @@ app.listen(8080, function(erro) {
   if(erro){
     console.log("Ocorreu um erro!");
   } else{
-    console.log("Servidor iniciado com sucesso");
+    console.log("Servidor iniciado com sucesso!");
   }
 });
