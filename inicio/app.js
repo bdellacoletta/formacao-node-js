41d5918b0824818bf667b15e7139fac12d73c032
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-08-01 11:57:39 -0300

    criando uma calculadora e importando módulos

---
+++ inicio/app.js
@@ -0,0 +1,8 @@
+var calculadora = require("./calculadora");
+
+
+console.log(calculadora.soma(1,20));
+console.log(calculadora.mult(1,20));
+console.log(calculadora.sub(1,20));
+console.log(calculadora.div(1,20));
+console.log(calculadora.nome);
