5af5146db3fe1e61ec57edceea9531db8ad66dd9
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-08-03 20:23:19 -0300

    adicionando condicional com ejs

--- plataforma-perguntas-respostas/index.js
+++ plataforma-perguntas-respostas/index.js
@@ -6,11 +6,13 @@ app.set('view engine', 'ejs'); //dizendo para o express usar o EJS como view eng
 app.get("/:nome/:language", (req, res) => {
   var nome = req.params.nome;
   var language = req.params.language;
+  var exibirMensagem = false;
   res.render('index', { //redireciona a página para o arquivo de html
     nome: nome,
     language: language,
     empresa: "Guia do programador",
-    inscritos: 8000
+    inscritos: 8000,
+    mensagem: exibirMensagem
   });
 });
 
