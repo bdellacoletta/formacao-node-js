e375616ee01e9c76732e07de8310a84aac2533f2
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-08-03 20:51:32 -0300

    linkando arquivos estáticos

--- plataforma-perguntas-respostas/index.js
+++ plataforma-perguntas-respostas/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs'); //dizendo para o express usar o EJS como view engine
+app.use(express.static('public'));
 
 app.get("/:nome/:language", (req, res) => {
   var nome = req.params.nome;
