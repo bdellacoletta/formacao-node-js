31fb3d14484263699e93c506c8d1987487c05619
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-08-03 21:49:50 -0300

    adicionado bootstrap

--- plataforma-perguntas-respostas/index.js
+++ plataforma-perguntas-respostas/index.js
@@ -4,27 +4,8 @@ const app = express();
 app.set('view engine', 'ejs'); //dizendo para o express usar o EJS como view engine
 app.use(express.static('public'));
 
-app.get("/:nome/:language", (req, res) => {
-  var nome = req.params.nome;
-  var language = req.params.language;
-  var exibirMensagem = false;
-
-  var produtos = [
-    {nome: "doritos", preco: 3.14},
-    {nome: "coca-cola", preco: 5},
-    {nome: "leite", preco: 1.45},
-    {nome: "carne", preco: 50},
-    {nome: "redbull", preco: 9}
-  ];
-
-  res.render('index', { //redireciona a página para o arquivo de html
-    nome: nome,
-    language: language,
-    empresa: "Guia do programador",
-    inscritos: 8000,
-    mensagem: exibirMensagem,
-    produtos: produtos
-  });
+app.get("/", (req, res) => {
+  res.render('index'); //redireciona a página para o arquivo de html
 });
 
 app.listen(8080, () => {
